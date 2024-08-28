83ffddde9e6e1b255dca5c79f8aca020e4eb8d28
Zach Heckert <zach@zachheckert.com>
2024-08-28 11:11:03 -0400

    Basic post constructed. Receiving and storing user input

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,4 +1,21 @@
+export async function POST({request}) {
+  // * Request body: original_url (the URL to be shortened)
+  // * Response: shortened_url (the newly generated shortened URL)
+
+  const data = await request.json();
+  const original_url = data.original_url;
+
+  return new Response(JSON.stringify({ shortened_url: original_url }), {
+    status: 201,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
 export async function GET() {
+  // * Request parameter: shortened_url (the shortened URL to redirect to)
+  // * Response: Redirect to the original URL
   return new Response(JSON.stringify({ message: 'Request received!' }), {
     status: 200,
     headers: {
