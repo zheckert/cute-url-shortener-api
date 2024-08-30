612cc471687be0fee3c91e40c4e7cfd467b8efe5
Zach Heckert <zach@zachheckert.com>
2024-08-29 20:57:49 -0400

    Flesh out post with placeholder random elements

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,11 +1,21 @@
-export async function POST({request}) {
-  // * Request body: original_url (the URL to be shortened)
-  // * Response: shortened_url (the newly generated shortened URL)
+export async function POST({ request }) {
+  // todo: remove later. Request body: original_url (the URL to be shortened)
+  // todo: remove later. Response: shortened_url (the newly generated shortened URL)
 
   const data = await request.json();
   const original_url = data.original_url;
 
-  return new Response(JSON.stringify({ shortened_url: original_url }), {
+  // Generate a random 6-character string
+  // todo: generate a UUID or something more unique.
+  const randomString = Math.random().toString(36).substring(2, 8);
+
+  // Construct the shortened URL 
+  // todo: set up a variable to handle prod OR sandbox
+  const shortenedUrl = `http://localhost:5173/${randomString}`;
+
+  return new Response(JSON.stringify({
+    message: `${original_url} is now ${shortenedUrl}`
+  }), {
     status: 201,
     headers: {
       'Content-Type': 'application/json',
@@ -14,8 +24,8 @@ export async function POST({request}) {
 }
 
 export async function GET() {
-  // * Request parameter: shortened_url (the shortened URL to redirect to)
-  // * Response: Redirect to the original URL
+  // todo: remove later. Request parameter: shortened_url (the shortened URL to redirect to)
+  // todo: remove later. Response: Redirect to the original URL
   return new Response(JSON.stringify({ message: 'Request received!' }), {
     status: 200,
     headers: {
