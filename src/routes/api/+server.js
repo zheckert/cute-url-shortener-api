573c70ffe30c44bf19f476852666a1120034517f
Zach Heckert <zach@zachheckert.com>
2024-09-04 16:20:25 -0400

    Add dependencies and functionality to handle env variables and uuids. Add try/catch blocks

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,35 +1,62 @@
+// Load environment variables
+import { config } from 'dotenv';
+config();
+
+// Instantiate uuid for help constructing unique URLs
+const { v4: uuidv4 } = await import('uuid');
+
+// Dynamically populate baseURL based on environment
+const isProd = process.env.APP_ENV === 'production';
+
 export async function POST({ request }) {
   // todo: remove later. Request body: original_url (the URL to be shortened)
   // todo: remove later. Response: shortened_url (the newly generated shortened URL)
 
-  const data = await request.json();
-  const original_url = data.original_url;
+  try {
+    const data = await request.json();
+    const original_url = data.original_url;
 
-  // Generate a random 6-character string
-  // todo: generate a UUID or something more unique.
-  const randomString = Math.random().toString(36).substring(2, 8);
+    if (!original_url) {
+      throw new Error('Missing original_url in request body');
+    }
 
-  // Construct the shortened URL 
-  // todo: set up a variable to handle prod OR sandbox
-  const shortenedUrl = `http://localhost:5173/${randomString}`;
+    const newId = uuidv4();
 
-  return new Response(JSON.stringify({
-    message: `${original_url} is now ${shortenedUrl}`
-  }), {
-    status: 201,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+    // Construct the shortened URL
+    const baseURL = isProd ? 'todo: populate with prod URL' : 'http://localhost:5173';
+    const shortenedUrl = `${baseURL}/${newId}`;
+
+    return new Response(JSON.stringify({
+      message: `${original_url} is now ${shortenedUrl}`
+    }), {
+      status: 201,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
 
 export async function GET() {
   // todo: remove later. Request parameter: shortened_url (the shortened URL to redirect to)
   // todo: remove later. Response: Redirect to the original URL
-  return new Response(JSON.stringify({ message: 'Request received!' }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  try {
+    // todo: come back to this!
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
